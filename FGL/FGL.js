a5f54747cdb2c5d5a89026ecf6687a524cf4c5e7
Ultima2876 <ultima2876@gmail.com>
2014-05-21 13:36:57 +0100

    -updated with DisabledButton class -removed extraneous functions -minor cleanup

--- FGL/FGL.js
+++ FGL/FGL.js
@@ -7,12 +7,14 @@
  */
  
  
- //PLUGIN SETUP
+/**
+ * Constructor
+ */
 Phaser.Plugin.FGL = function (game, parent) {
   Phaser.Plugin.call(this, game, parent);
   
   // Setup: 
-  if(('__fgl' in window) == false) {
+  if(('fgl' in window) == false) {
     throw new Error('You need to include the FGL SDK in your project, add the following script tag to your HTML file:\n<script src="https://sites.mpstatic.com/html5/sdks/1.4.2/fgl.js"></script>');
   }
   
@@ -21,9 +23,10 @@ Phaser.Plugin.FGL = function (game, parent) {
   game.fgl = this;
   
   // Auto-load branding:
-  if(window.__fgl['brandingEnabled']){
-    var loader = new Phaser.Loader(game)
-    loader.image('fgl-branding-image', window.__fgl['getBrandingLogo']());
+  if(window.fgl['brandingEnabled']){
+    var loader = new Phaser.Loader(game);
+    loader.image('fgl-branding-image', window.fgl['getBrandingLogo']());
+    console.log(window.fgl['getBrandingLogo']());
     loader.start();
   }
   
@@ -31,6 +34,17 @@ Phaser.Plugin.FGL = function (game, parent) {
 
 Phaser.Plugin.FGL.prototype = Object.create(Phaser.Plugin.prototype);
 Phaser.Plugin.FGL.prototype.constructor = Phaser.Plugin.FGL;
+
+// Special button that is always invisible and has no pressed callback
+DisabledButton = function (game, x, y, key, callback, callbackContext, overFrame, outFrame, downFrame, upFrame) {
+  Phaser.Button.call(this, game, x, y, key, null, callbackContext, overFrame, outFrame, downFrame, upFrame);
+  this.visible = false;
+}
+DisabledButton.prototype = Object.create(Phaser.Button.prototype);
+DisabledButton.prototype.constructor = DisabledButton;
+Object.defineProperty(DisabledButton.prototype, 'visible', {
+  get: function() { return false; }, set: function(val) { }
+});
 
 /**
  * Creates a new FGL Publisher Branding button object.
@@ -43,12 +57,12 @@ Phaser.Plugin.FGL.prototype.constructor = Phaser.Plugin.FGL;
  * @return {Phaser.Button} The newly created button object.
  */
 Phaser.GameObjectFactory.prototype.fglPublisherBranding = function (x, y, width, group) {
-  if (!window.__fgl['brandingEnabled']) return {destroy: function(){}};
-  
   if (typeof group === 'undefined') { group = this.world; }
   width = width || 250;
   
-  var button = group.add(new Phaser.Button(this.game, x, y, 'fgl-branding-image', window.__fgl['handleBrandingClick']));
+  // If fgl branding is not enabled, return a disabled button that never renders or raises a pressed callback
+  if (!window.fgl['brandingEnabled']) { return group.add(new DisabledButton(this.game, x, y, 'fgl-branding-image', null)); };
+  var button = group.add(new Phaser.Button(this.game, x, y, 'fgl-branding-image', window.fgl['handleBrandingClick']));
   
   button.width = width;
   button.height = width * (100/250);
@@ -71,10 +85,11 @@ Phaser.GameObjectFactory.prototype.fglPublisherBranding = function (x, y, width,
  * @return {Phaser.Button} The newly created button object.
  */
 Phaser.GameObjectFactory.prototype.fglMoreGamesButton = function (x, y, key, overFrame, outFrame, downFrame, upFrame, group) {
-  if (!window.__fgl['crossPromotionEnabled']) return {destroy: function(){}};
   if (typeof group === 'undefined') { group = this.world; }
+  
+  if (!window.fgl['crossPromotionEnabled']) { return group.add(new DisabledButton(this.game, x, y, key, null)); };
 
-  return group.add(new Phaser.Button(this.game, x, y, key, window.__fgl['showMoreGames'], null, overFrame, outFrame, downFrame, upFrame));
+  return group.add(new Phaser.Button(this.game, x, y, key, window.fgl['showMoreGames'], null, overFrame, outFrame, downFrame, upFrame));
 };
 
 /**
@@ -94,113 +109,12 @@ Phaser.GameObjectFactory.prototype.fglMoreGamesButton = function (x, y, key, ove
  * @return {Phaser.Button} The newly created button object.
  */
 Phaser.GameObjectFactory.prototype.fglUpgradeButton = function (x, y, key, successCallback, failCallback, overFrame, outFrame, downFrame, upFrame, group) {
-  if (!window.__fgl['unlockEnabled']) return {destroy: function(){}};
-  if (window.fgl['isPremium']()) return {destroy: function(){}};
-  
   if (typeof group === 'undefined') { group = this.world; }
-
+  
+  if (!window.fgl['unlockEnabled']) { return group.add(new DisabledButton(this.game, x, y, key, null)); };
+  if (window.fgl['isPremium']()) { return group.add(new DisabledButton(this.game, x, y, key, null)); };
+  
   return group.add(new Phaser.Button(this.game, x, y, key, function(){ 
-    window.__fgl['inApp']['initiateUnlockFunction'](successCallback, failCallback);
+    window.fgl['inApp']['initiateUnlockFunction'](successCallback, failCallback);
   }, null, overFrame, outFrame, downFrame, upFrame));
-};
-
-// SDK FUNCTIONS
-/**
- * Report current game state
- * @param {string} state game state to report
- */
-Phaser.Plugin.FGL.prototype.reportGameState = function (state) {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['reportGameState'](state);
-};
-
-/**
- * Request quit to shut down cleanly
- */
-Phaser.Plugin.FGL.prototype.requestQuit = function () {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['requestQuit']();
-};
-
-/**
- * Show an advertisement
- * @param {Object} options options to send to ad server. Can be omitted
- */
-Phaser.Plugin.FGL.prototype.showAd = function (options) {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['showAd'](options);
-};
-
-/**
- * Submits the given score to the specified leaderboard. If your game
- * only has one leaderboard, do not supply a leaderboardID.
- * @param {Number} score Score to submit
- * @param {string} leaderboardID Name of the leaderboard to show
- */
-Phaser.Plugin.FGL.prototype.submitScore = function(score, leaderboardID, extra) {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['submitScore'](score, leaderboardID, extra);
-};
-
-/**
- * Displays the scoreboard UI over your game
- * @param {string} leaderboardID Name of the leaderboard to show
- * @param {Number} highlightScore Index of score in the list to highlight (0 for none)
- */
-Phaser.Plugin.FGL.prototype.displayScoreboard = function(leaderboardID, highlightScore) {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['displayScoreboard'](leaderboardID, highlightScore);
-};
-
-/**
- * Forcibly removes the scoreboard UI from your game. This should not be
- * necessary (the player can easily dismiss all overlays)
- */
-Phaser.Plugin.FGL.prototype.hideScoreboard = function(e) {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['hideScoreboard'](e);
-};
-
-/**
- * Grants a specified achievement to the player
- * @param {string} achievementId achievement ID to grant to the player
- */
-Phaser.Plugin.FGL.prototype.grantAchievement = function(achievementId) {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['grantAchievement'](achievementId);
-};
-
-/**
- * Show achievements the player has unlocked
- */
-Phaser.Plugin.FGL.prototype.showAchievements = function() {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['showAchievements']();
-};
-
-/**
- * Get whether a particular achievement has been unlocked
- * @param {string} achievementId achievement ID to grant to the player
- * @returns {boolean} true if the achievement has been unlocked
- */
-Phaser.Plugin.FGL.prototype.hasAchievement = function(achievementId) {
-  if ( ! ('__fgl' in window)) return false;
-  return __fgl['hasAchievement'](achievementId);
-};
-
-/**
- * Check whether we are running the premium (no ads) version of the game
- * @returns {boolean} true if the game is running in premium mode
- */
-Phaser.Plugin.FGL.prototype.isPremium = function() {
-  if ( ! ('__fgl' in window)) return false;
-  return __fgl['isPremium']();
-};
-
-/**
- * Show the 'more games' cross promotion overlay
- */
-Phaser.Plugin.FGL.prototype.showMoreGames = function() {
-  if ( ! ('__fgl' in window)) return;
-  __fgl['showMoreGames']();
 };
